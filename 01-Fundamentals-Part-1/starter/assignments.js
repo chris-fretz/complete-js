4e9fa6f1d6640773e1f1f2e2f3a3c63688365002
Chris Fretz <chrisfretz@gmail.com>
2025-09-29 22:53:43 -0400

    Add assignments.js

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,10 @@
+// From: https://jonas.io/assignments/values-and-variables.html
+
+// Assignement: Values & Variables
+let country = "United States";
+let continent = "North America";
+let population = 342;
+
+console.log(country);
+console.log(continent);
+console.log(population);
