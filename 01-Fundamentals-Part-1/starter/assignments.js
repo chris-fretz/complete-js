3c3bcb1b3e3e9fe8188de0fcc2103673017f3767
Chris Fretz <chrisfretz@gmail.com>
2025-10-01 22:25:59 -0400

    Do assignments for data types, variable types, and basic operators

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,10 +1,38 @@
 // From: https://jonas.io/assignments/values-and-variables.html
 
 // Assignement: Values & Variables
-let country = "United States";
-let continent = "North America";
+const country = "United States";
+const continent = "North America";
 let population = 342;
 
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// Assignment: Data Types
+const isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+// Assignment: let, const, and var
+language = "English";
+
+// Assignment: Basic Operators
+const populationHalved = population / 2;
+console.log(populationHalved);
+
+const populationPlusOne = population++;
+console.log(populationPlusOne);
+
+populationComparedWithFinland = population > 6;
+console.log(populationComparedWithFinland);
+
+const populationMoreThanAvg = population < 33;
+console.log(populationMoreThanAvg);
+
+const description = country + " is in " + continent + ", and its " + population + " million people speak " + language;
+console.log(description);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,3 +3,6 @@ if (js === "amazing") alert("JavaScript is FUN!");
 
 40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+// 2 ** 3 means 2 to the power of 3, aka 2 * 2 * 2
+console.log(2 ** 3);
