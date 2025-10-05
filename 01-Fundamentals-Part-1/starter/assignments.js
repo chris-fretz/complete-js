e0ef5dd44a6f96e3a4bff80c338bee46482b633e
Chris Fretz <chrisfretz@gmail.com>
2025-10-04 21:25:31 -0400

    Complete if else assignment

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -35,15 +35,33 @@ const populationMoreThanAvg = population < 33;
 // console.log(populationMoreThanAvg);
 
 const description = country + " is in " + continent + ", and its " + population + " million people speak " + language;
-console.log(description);
+// console.log(description);
 
 // ASSIGNMENT: Strings & Template Literals
 const descriptionUpdated = `${country} is in ${continent} and it's ${population} million people speak ${language}.`;
-console.log(descriptionUpdated);
+// console.log(descriptionUpdated);
 
 // ASSIGNMENT: if/else statements
 if(population > 33) {
-    console.log(`${country}'s population is above average.`);
+    // console.log(`${country}'s population is above average.`);
 } else {
-    console.log(`${country}'s population is ${33 - population} million below average.`);
+    // console.log(`${country}'s population is ${33 - population} million below average.`);
+}
+
+// ASSIGNMENT: Equality Operators: == vs. ===
+/* const numNeighbors = Number(prompt('How many neighbour countries does your country have?'));
+
+if (numNeighbors === 1) {
+    console.log('Only 1 border');
+} else if (numNeighbors > 1) {
+    console.log('More than 1 border');
+} else {
+    console.log('No borders');
+} */
+
+// ASSIGNMENT: Logical Operators
+if (language === 'English' && population < 50 && !isIsland) {
+    console.log('You should live in the U.S. :)');
+} else {
+    console.log('The U.S. does not meet your criteria :(');
 }
