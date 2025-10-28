2cf71873011b88885d4d94673809c04d8a7f0950
Chris Fretz <chris@wtmdigital.com>
2025-10-27 20:36:29 -0400

    Add notes for lessons on functions (general) and function declarations vs. function expressions

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -1,2 +1,12 @@
 // From: https://jonas.io/assignments/instructions.html
 
+// ASSIGNMENT: Functions
+function describeCountry(country, population, capitalCity) {
+   return `${country} has ${population} million people and its capital city is ${capitalCity}.`;return `${country} has ${population} million people and its capital city is ${capitalCity}.`;
+}
+
+const desc1 = describeCountry('Finland', 6, 'Helsinki');
+const desc2 = describeCountry('India', 1380, 'New Delhi');
+const desc3 = describeCountry('Canada', 38, 'Ottawa');
+
+console.log(desc1, desc2, desc3);
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,9 +1,49 @@
 'use strict';
 
+// Lesson: Activating Strict Mode
+/*
 let hasDriversLicense = true;
 const passTest = true;
 
 /* Note that hasDriverLicense is actually singular, introducing a bug.
    This will actually throw an error if we use strict mode, instead of failing silently. */
+/*
 if (passTest) hasDriverLicense = true;
 if (hasDriversLicense) console.log('I can drive');
+*/
+
+// Lesson: Functions
+/*
+function logger() {
+    console.log('My name is Jonas');
+}
+
+// calling / running / invoking the function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+   console.log(apples, oranges);
+   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+   return juice;
+}
+*/
+
+// Lesson: Function Declarations vs. Expressions
+// Function Declaration: A named function defined with the function keyword
+// Can be called before it's declared and defined due to hoisting
+function calcAge1(birthYear) {
+   return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+console.log(age1);
+
+// Function Expression: An anonymous function which is an expression that returns a value and is stored in a variable
+const calcAge2 = function (birthYear) {
+   return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1991);
+console.log(age2);
