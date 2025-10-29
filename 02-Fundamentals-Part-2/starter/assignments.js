5afffe26688b6649d6ee35004a95ff5554b01275
Chris Fretz <chris@wtmdigital.com>
2025-10-29 10:44:54 -0400

    Complete lesson on functions calling other functions

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -9,7 +9,7 @@ const desc1 = describeCountry('Finland', 6, 'Helsinki');
 const desc2 = describeCountry('India', 1380, 'New Delhi');
 const desc3 = describeCountry('Canada', 38, 'Ottawa');
 
-console.log(desc1, desc2, desc3);
+// console.log(desc1, desc2, desc3);
 
 // ASSIGNMENT: Function Declarations vs. Expressions
 function percentageOfWorld1(population) {
@@ -20,7 +20,7 @@ const USAPercentage1 = percentageOfWorld1(331);
 const ChinaPercentage1 = percentageOfWorld1(1441);
 const NigeriaPercentage1 = percentageOfWorld1(206);
 
-console.log(USAPercentage1, ChinaPercentage1, NigeriaPercentage1);
+// console.log(USAPercentage1, ChinaPercentage1, NigeriaPercentage1);
 
 const percentageOfWorld2 = function (population) {
    return (population / 7900) * 100;
@@ -30,6 +30,14 @@ const percentageOfWorld2 = function (population) {
 const percentageOfWorld3 = population => (population / 7900) * 100;
 
 const BrazilPercentage3 = percentageOfWorld3(213);
-console.log(BrazilPercentage3);
+// console.log(BrazilPercentage3);
 
 // ASSIGNMENT: Functions Calling Other Functions
+const describePopulation = function (country, population) {
+   const percentage = percentageOfWorld1(population);
+   return `${country} has ${population} million people, which is about ${percentage.toFixed(2)}% of the world.`;
+}
+
+console.log(describePopulation('China', 1441));
+console.log(describePopulation('USA', 331));
+console.log(describePopulation('Nigeria', 206));
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -55,6 +55,7 @@ console.log(age2);
 // Arrow Function: A shorter syntax for writing function expressions whose value is returned implicitly
 // No need for the function keyword, return keyword, or curly braces for single-line functions
 // Arrow functions do not have their own 'this' keyword
+/*
 const calcAge3 = birthYear => 2037 - birthYear;
 const age3 = calcAge3(1991);
 console.log(age3);
@@ -66,3 +67,18 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 }
 
 console.log(yearsUntilRetirement(1991, 'Jonas'));
+*/
+
+// Lesson: Functions Calling Other Functions
+function cutFruitPieces(fruit) {
+   return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+   const applePieces = cutFruitPieces(apples);
+   const orangePieces = cutFruitPieces(oranges);
+   const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+   return juice;
+}
+
+console.log(fruitProcessor(2, 3));
